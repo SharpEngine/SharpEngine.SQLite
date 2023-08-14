055ba7a95484bc1ca43942735e598ea432b891b3
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-08-14 17:42:38 +0200

    Create SharpEngine.SQLite

---
+++ README.md
@@ -0,0 +1,13 @@
+# SharpEngine.SQLite
+
+SQLite Package for SharpEngine - C# 2D Game Engine
+
+## Installation, Download, Usage
+
+*Coming Soon*
+
+## Dependencies
+
+- Net7
+- SharpEngine.Core
+- System.Data.SQLite
---
+++ SharpEngine.SQLite/SESQLite.cs
@@ -0,0 +1,19 @@
+using SharpEngine.Core.Manager;
+
+namespace SharpEngine.SQLite;
+
+/// <summary>
+/// Static class with extensions and add version functions
+/// </summary>
+public class SESQLite
+{
+    /// <summary>
+    /// Add versions to DebugManager
+    /// </summary>
+    public static void AddVersions()
+    {
+        DebugManager.Versions.Add("System.Data.SQLite", "1.0.118");
+        DebugManager.Versions.Add("SharpEngine.SQLite", "1.0.0");
+    }
+    
+}
